f4ae83d0c432963aaf052f304efd7e273552a844
38058990 <38058990@qq.com>
2017-04-14 14:24:18 +0800

    Zhxin2.0
    手机端交互版

--- js/js.js
+++ js/js.js
@@ -1,11 +1,9 @@
 $(function(){
-    
-    
         $(window).scroll(function(){
             var w_h = $(window).scrollTop();
             if(w_h >= 0){
                 $(".shade-font").fadeIn(1000);
-            }if(w_h >100){
+            }if(w_h >300){
                 $(".mater_a-font,.mater-r").fadeIn(1000);
             }if(w_h >900){
                 $(".object,.sub-in").fadeIn(1000);
@@ -13,15 +11,8 @@ $(function(){
                 $(".sevr-tit,.sevr-first,.sevr-second,.sevr-third,.sevr-fouth").fadeIn(1000);
             }if(w_h > 2800){
                 $(".base-l,.base-r").fadeIn(1000);
-            }if(w_h > 600){
-                $(".back-ico,.return").show();
-            }else{
-                $(".back-ico,.return").hide();
             }
         })
-        
-        
-        
         $(".menu-sm").on("click",function(){
        if($(this).hasClass("h")){
 			$(this).removeClass("h");
@@ -29,30 +20,4 @@ $(function(){
 			$(this).addClass("h");
 		} 
 	})
-        $(window).scroll(function(){
-            $(".menu-sm").addClass("h");
-        })
-        
-        
-        
-        
-           
-
-        $(".eng").on("click",function(){
-             $(".map").fadeIn();
-             $("#allmap").fadeIn();
-                 // 百度地图API功能  
-            var map = new BMap.Map("allmap");
-            var point = new BMap.Point(118.786511,32.029147);
-            map.centerAndZoom(point, 19);
-            var marker = new BMap.Marker(point);  // 创建标注
-            map.addOverlay(marker);               // 将标注添加到地图中
-            marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
-            map.enableScrollWheelZoom();   //启用滚轮放大缩小，默认禁用
-	        map.enableContinuousZoom();    //启用地图惯性拖拽，默认禁用
-        })
-            $(".close").on("click",function(){
-                $(".map").hide();
-                $("#allmap").hide();
-        })
 })
